b691315bc36ac6f022cc0d51a655e6ce3efa0ec4
Emily Wirtz <ewirtz3@gmail.com>
2021-04-25 13:38:42 -0500

    pushing changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-export default {
+const getUsers = {
   // Gets all users
   getUsers: function () {
     return axios.get("https://randomuser.me/api/?results=200&nat=us");
   },
 };
+
+export default getUsers;
